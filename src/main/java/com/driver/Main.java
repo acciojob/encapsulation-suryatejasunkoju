d843dd53e66a2c984288135a98cb236c53291122
suryatejasunkoju <sunkojusuryateja@gmail.com>
2023-01-16 18:07:01 +0530

    done with code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,9 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        com.driver.RWOnly r=new com.driver.RWOnly();
+        r.setName("lkjhg");
+        System.out.println(r.getName());
+    }
 }
