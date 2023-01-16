aac7505112cee664f5402a178be169a3a317681b
Suryateja Sunkoju <sunkojusuryateja@gmail.com>
2023-01-16 18:19:47 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         com.driver.RWOnly r=new com.driver.RWOnly();
         r.setName("lkjhg");
+        //printing
         System.out.println(r.getName());
     }
 }
